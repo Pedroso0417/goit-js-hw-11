0aa0a04044bf2f7434fe0907382ecd9356107351
Pedroso0417/goit-js-hw-8 <136574676+Pedroso0417@users.noreply.github.com>
2024-01-12 15:38:50 +0800

    commit codes

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,13 @@
-// import axios from 'axios';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { BASE_URL, options } from './pixabay api.js';
-
-// const galleryElement = document.querySelector('.gallery');
-// const searchInputElement = document.querySelector('input[name="searchQuery"');
-// const searchFormElement = document.querySelector('search-form');
-
-// const lightbox = new SimpleLightbox('.lightbox', {
-//   captions: true,
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
-
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { BASE_URL, options } from './pixabay api.js';
+import { BASE_URL, options } from './pixabay_api.js'; 
+
 
 const galleryElement = document.querySelector('.gallery');
 const searchInputElement = document.querySelector('input[name="searchQuery"]');
-const searchFormElement = document.querySelector('search-form');
+const searchFormElement = document.querySelector('.search-form');
 
 const lightbox = new SimpleLightbox('.lightbox', {
   captions: true,
@@ -99,7 +84,7 @@ searchFormElement.addEventListener('submit', function (event) {
   if (searchQuery !== '') {
     fetchImages(searchQuery);
   } else {
-    Notify.info('Please enter a search query.');
+    Notify.info('Please enter a search query');
   }
 });
 
